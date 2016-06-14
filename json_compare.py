857363f07f0d20d5cca650696725a3ce259eb976
ohaz <thekilleddeath@endless-aerospace.de>
2016-06-14 11:52:58 +0200

    Add range to json compare

--- json_compare.py
+++ json_compare.py
@@ -4,12 +4,13 @@ from pprint import pprint
 __author__ = 'ohaz'
 
 
-def dict_compare(dict1, dict2):
+def dict_compare(dict1, dict2, factor):
     similar = []
     for k, v in dict1.items():
-        if v in dict2.values():
-            k2 = list(dict2.keys())[list(dict2.values()).index(v)]
-            similar.append((k, k2, v))
+        for nv in range(max(0, v-int(v/factor)), v+int(v/factor)):
+            if nv in dict2.values():
+                k2 = list(dict2.keys())[list(dict2.values()).index(nv)]
+                similar.append((k, k2, v, nv))
     return similar
 
 if __name__ == '__main__':
@@ -19,4 +20,4 @@ if __name__ == '__main__':
     with open(sys.argv[2]) as d2_file:
         content = d2_file.read()
         d2 = json.loads(content)
-    pprint(dict_compare(d1, d2))
+    pprint(dict_compare(d1, d2, factor=20))
